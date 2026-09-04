132770d07067e9d1bcb18faef8f332592b68b744
Tulajaram12 <96332761+Tulajaram12@users.noreply.github.com>
2026-09-04 21:36:17 +0530

    Create NotificationRepository interface for notifications

---
+++ src/main/java/com/ecommerce/notificationservice/repository/NotificationRepository.java
@@ -0,0 +1,14 @@
+package com.ecommerce.notificationservice.repository;
+
+import com.ecommerce.notificationservice.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository
+        extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByUserId(Long userId);
+
+    List<Notification> findByOrderId(Long orderId);
+}
