678f0dd5ddadb923bf578116e90a2852d726bd2b
Tulajaram12 <96332761+Tulajaram12@users.noreply.github.com>
2026-09-04 21:35:40 +0530

    Create NotificationRequest.java

---
+++ src/main/java/com/ecommerce/notificationservice/dto/NotificationRequest.java
@@ -0,0 +1,47 @@
+package com.ecommerce.notificationservice.dto;
+
+public class NotificationRequest {
+
+    private Long userId;
+
+    private Long orderId;
+
+    private String type;
+
+    private String message;
+
+    public NotificationRequest() {
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
